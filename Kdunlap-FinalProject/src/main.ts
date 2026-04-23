87a7381eef7ef6b7c5767b25f205a7e3bbc28fca
Kip <Kipling.Dunlap@gmail.com>
2026-04-23 19:48:11 -0400

    basic directions working!!!

--- Kdunlap-FinalProject/src/main.ts
+++ Kdunlap-FinalProject/src/main.ts
@@ -1,17 +1,17 @@
 import "./style.css";
 
 // Optional: If you're loading secure web maps
-// import { configureOAuth } from "./auth/configureOAuth";
-// configureOAuth({
-//   // Default portalUrl is ArcGIS Online
-//   // Only set if using other portals
-//   portalUrl: "YOUR_PORTAL_URL",
-//   appId: "YOUR_APP_ID",
-// });
+import { configureOAuth } from "./auth/configureOAuth";
+configureOAuth({
+  // Default portalUrl is ArcGIS Online
+  // Only set if using other portals
+  // portalUrl: "gsu.maps.arcgis.com",
+  appId: "TBZsNEh4voqSgzJj",
+});
 
 // Individual imports for each Map, Chart and Calcite component
 import "@arcgis/map-components/components/arcgis-expand";
-import "@arcgis/map-components/components/arcgis-legend";
+import "@arcgis/map-components/components/arcgis-directions";
 import "@arcgis/map-components/components/arcgis-map";
 import "@arcgis/map-components/components/arcgis-search";
 import "@arcgis/map-components/components/arcgis-zoom";
@@ -19,6 +19,7 @@ import "@arcgis/charts-components/components/arcgis-chart";
 import "@esri/calcite-components/components/calcite-shell";
 import "@esri/calcite-components/components/calcite-navigation";
 import "@esri/calcite-components/components/calcite-navigation-logo";
+
 
 // Import modules and types from the SDK's core API
 import Graphic from "@arcgis/core/Graphic.js";
@@ -35,39 +36,39 @@ viewElement?.addEventListener("arcgisViewReadyChange", () => {
   const map = viewElement.map as WebMap;
   const portalItem = map.portalItem;
   const title = portalItem?.title ? portalItem.title : "A web map";
-  const description = portalItem?.description ? portalItem.description : "ArcGIS Maps SDK for JavaScript template";
+  // const description = portalItem?.description ? portalItem.description : "ArcGIS Maps SDK for JavaScript template";
 
   if (calciteNavLogo) {
     calciteNavLogo.heading = title;
-    calciteNavLogo.description = description;
+    // calciteNavLogo.description = description;
   }
 
   // Define a point geometry
-  const point = new Point({
-    longitude: -98.38,
-    latitude: 38.34,
-  });
+  // const point = new Point({
+  //   longitude: -98.38,
+  //   latitude: 38.34,
+  // });
 
   // Create an outline for the marker symbol
-  const outline = new SimpleLineSymbol({
-    color: "white",
-    width: 2,
-  });
+  // const outline = new SimpleLineSymbol({
+  //   color: "white",
+  //   width: 2,
+  // });
 
   // Create a symbol for drawing the point
-  const symbol = new SimpleMarkerSymbol({
-    style: "triangle",
-    size: 20,
-    color: "red",
-    outline,
-  });
+  // const symbol = new SimpleMarkerSymbol({
+  //   style: "triangle",
+  //   size: 20,
+  //   color: "red",
+  //   outline,
+  // });
 
   // Create a graphic and add the geometry and symbol to it
-  const pointGraphic = new Graphic({
-    geometry: point,
-    symbol,
-  });
+  // const pointGraphic = new Graphic({
+  //   geometry: point,
+  //   symbol,
+  // });
 
   // Add a graphic to demonstrate custom visualizations beyond Web Map content
-  viewElement.graphics.add(pointGraphic);
+  // viewElement.graphics.add(pointGraphic);
 });
