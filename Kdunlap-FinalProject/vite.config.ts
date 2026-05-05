305365c11771d11bb566ddaa07b3ee72ee98b26e
Kip <Kipling.Dunlap@gmail.com>
2026-05-05 11:21:51 -0400

    nvm

--- Kdunlap-FinalProject/vite.config.ts
+++ Kdunlap-FinalProject/vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 
-const apiKey = import.meta.env.apiKey;
+// const apiKey = import.meta.env.apiKey;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
   server: {
     open: true,
-    proxy: {
-      '/api': {
-        target: `https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/developerservices/traindata?apiKey=${apiKey}`,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+    // proxy: {
+    //   '/api': {
+    //     target: `https://developerservices.itsmarta.com:18096/itsmarta/railrealtimearrivals/developerservices/traindata?apiKey=${apiKey}`,
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ''),
+    //   },
     }
   },
   build: {
